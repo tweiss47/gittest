5d36d841415b255fd182db5f974f38bfab005d42
tweiss47 <tweiss47@users.noreply.github.com>
2017-06-01 17:37:27 -0700

    Added a comment

--- src/gittest/Hello.java
+++ src/gittest/Hello.java
@@ -1,6 +1,8 @@
+
 package gittest;
 
 public class Hello {
+	// A comment
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
 		System.out.println("Hello, world is such a cliche.");
